61cc9abc3ab1f2c2cb9b357aafb0281d60d2588b
Jake Noe <67278790+jpnoe@users.noreply.github.com>
2020-08-27 14:45:44 -0700

    Update Cell.swift

--- Source/Core/Cell.swift
+++ Source/Core/Cell.swift
@@ -125,9 +125,9 @@ open class Cell<T>: BaseCell, TypedCellType where T: Equatable {
         super.update()
         textLabel?.text = row.title
         if #available(iOS 13.0, *) {
-            textLabel?.textColor = .tertiaryLabel
+            textLabel?.textColor = row.isDisabled ? .tertiaryLabel : .label
         } else {
-            textLabel?.textColor = .gray
+            textLabel?.textColor = row.isDisabled ? .gray : .black
         }
         detailTextLabel?.text = row.displayValueFor?(row.value) ?? (row as? NoValueDisplayTextConformance)?.noValueDisplayText
     }
